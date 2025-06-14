3d61dfe64bd659519c5136c32a2b35d9d5ef58fc
caarlosandree <caarlosandree@gmail.com>
2025-06-13 21:47:46 -0300

    feat: ajuste nos icones das redes sociais

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 // src/components/Footer.jsx
 import React from 'react';
 import './Footer.css';
+import {FaGithub, FaInstagram, FaLinkedin, FaWhatsapp} from "react-icons/fa";
 
 const Footer = () => {
     return (
@@ -15,9 +16,26 @@ const Footer = () => {
                 </p>
                 <h2>Redes Sociais</h2>
                 <ul className="social-links">
-                    <li><a href="https://linkedin.com/in/caarlosandree" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
-                    <li><a href="https://github.com/caarlosandree" target="_blank" rel="noopener noreferrer">GitHub</a></li>
-                    <li><a href="https://instagram.com/carlosasab" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+                    <li>
+                        <a href="https://linkedin.com/in/caarlosandree" className="linkedin-icon" target="_blank" rel="noopener noreferrer" aria-label="Acessar meu perfil no LinkedIn" title="LinkedIn">
+                            <FaLinkedin />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/caarlosandree" className="github-icon" target="_blank" rel="noopener noreferrer" aria-label="Acessar meu perfil no Github" title="Github">
+                            <FaGithub />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://instagram.com/carlosasab" className="instagram-icon" target="_blank" rel="noopener noreferrer" aria-label="Acessar meu perfil no Instagram" title="Instagram">
+                            <FaInstagram />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://wa.me/5548999244627" className="whatsapp-icon" target="_blank" rel="noopener noreferrer" aria-label="Me chame no WhatsApp" title="WhatsApp">
+                            <FaWhatsapp />
+                        </a>
+                    </li>
                 </ul>
             </section>
             <hr />
